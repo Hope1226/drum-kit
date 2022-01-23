6b49776076098afed2e3ec2845d15a184549a1a5
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-01-23 21:25:43 +0500

    Add keyboard and click play feature

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ const keys = document.querySelectorAll(".keys");
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
     const audio = document.querySelector(`audio[data-key="${e.target.id}"]`)
-    setInterval(() => {
-      audio.play()
-    }, 2000)
+    const key = document.querySelector(`.keys[data-key="${e.target.id}"]`);
+    audio.play()
+    key.classList.add('playing');
+    setTimeout(() => {
+    key.classList.remove('playing')
+    }, 100)
   })
 })
 
 window.addEventListener('keydown', (e) => {
   const auido = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.keys[data-key="${e.keyCode}"]`);
   if(!auido) return;
   auido.currentTime = 0;
   auido.play();
-  
-  console.log(auido)
+  key.classList.add('playing');
+  setTimeout(() => {
+    key.classList.remove('playing')
+  }, 100)    
 })                                   
